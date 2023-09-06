01e5b88dd741b6aa672776b4976412893f9db308
Yakov Damen <yakov.damen@gmail.com>
2023-09-06 17:08:14 +0300

    added user registration and login

--- src/components/UserLogin.tsx
+++ src/components/UserLogin.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
 
-export default function UserLogin() {
+export default function UserRegistration() {
+  const navigate = useNavigate()
+  const { register, handleSubmit } = useForm()
+
+  const updateFetch = async (body: FieldValues) => {
+    const res = await fetch(`http://localhost:3000/api/auth/login`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    if (res.ok) navigate(`/`)
+    const resBody = await res.json()
+    localStorage.setItem('token', resBody.responseObj.token)
+    
+
+  }
+
   return (
-    <div>UserLogin</div>
+    <><Link to={'/'}><button>Home</button></Link>
+
+      <form onSubmit={handleSubmit((data) => updateFetch(data))}>
+        <h2>Login</h2>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" {...register('email')} />
+          <label htmlFor="password">Password</label>
+          <input type="password" {...register('password')} />
+        </div>
+        <button type='submit'>Submit</button>
+      </form></>
   )
 }
--- src/components/UserRegistration.tsx
+++ src/components/UserRegistration.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { FieldValues, useForm } from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function UserRegistration() {
+  const navigate = useNavigate()
+  const { register, handleSubmit } = useForm()
+
+  const updateFetch = async (body: FieldValues) => {
+    const res = await fetch(`http://localhost:3000/api/auth/register`, {
+      method: 'POST',
+      headers: {
+        authorization: 'test-token',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+    if (res.ok) navigate(`/`)
+  }
+
   return (
-    <div>UserRegistration</div>
+    <><Link to={'/'}><button>Home</button></Link>
+
+      <form onSubmit={handleSubmit((data) => updateFetch(data))}>
+        <h2>Registeration</h2>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" {...register('email')} />
+          <label htmlFor="password">Password</label>
+          <input type="password" {...register('password')} />
+        </div>
+        <button type='submit'>Submit</button>
+      </form></>
   )
 }
